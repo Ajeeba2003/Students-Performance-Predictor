351cf9261d81a1a2db7daa4bf58f4632aa06ee2f
Ajeeba2003 <ajeebamh25@gmail.com>
2025-02-18 12:52:19 +0530

    StudentApp

---
+++ studentApp.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import joblib
+import pandas as pd 
+
+
+encode=joblib.load('Encoder.pkl')
+ss=joblib.load('scaler.pkl')
+model=joblib.load('Student_Performance.pkl')
+
+
+st.title('Student Performance Prediction')
+st.write('This app predicts student performance using data-driven insights for better learning outcomes!')
+Hour_studied=st.number_input('How many hours did the student study per day?')
+score=st.number_input("Enter the student's past academic scores")
+sleep_hour=st.number_input('How many hours does the student sleep?')
+sample_paper=st.number_input('How many sample question papers has the student practiced?')
+eca=st.selectbox('Has the student been involved in any extracurricular activities?',('Yes',"No"),placeholder="Select Option")
+eca=encode.transform([eca])
+
+data=pd.DataFrame({'Hours Studied':Hour_studied, 'Previous Scores':score, 'Sleep Hours':sleep_hour, 'Sample Question Papers Practiced':sample_paper,'Encoded':eca})
+scale=ss.transform(data)
+Prediction=model.predict(scale)
+
+
+if st.button('Predict Performance'):
+    if Prediction[0]>60:
+        st.success(f"Student's Performance Prediction is {round(Prediction[0],2)}")
+    elif Prediction[0]>40:
+        st.warning(f"Student's Performance Prediction is {round(Prediction[0],2)}")
+    else :
+        st.error(f"Student's Performance Prediction is {round(Prediction[0],2)}")
